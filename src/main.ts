b8b098c7802e2e68b79420f51793bd282653ea4e
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2025-12-26 15:27:52 +0100

    Milestone 1 - 5 no bonus

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,65 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type Nationality =
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese"
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string]
+  awards: string,
+  nationality: Nationality
+}
+
+function isActress(data: any): data is Actress {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    typeof data.id === "number" &&
+    typeof data.name === "string" &&
+    typeof data.birth_year === "number" &&
+    typeof data.biography === "string" &&
+    typeof data.image === "string" &&
+    Array.isArray(data.most_famous_movies) &&
+    data.most_famous_movies.length === 3 &&
+    typeof data.awards === "string" &&
+    typeof data.nationality === "string"
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  const response = await fetch(`http://localhost:3333/actresses/${id}`)
+
+  if (!response.ok) return null
+
+  const data = await response.json()
+
+  return isActress(data) ? data : null
+}
+
+async function getAllActresses(): Promise<Actress[]> {
+  const response = await fetch("http://localhost:3333/actresses")
+  const actresses: Actress[] = await response.json()
+  return actresses
+}
+
+async function getActresses(ids: number[]): Promise<(Actress | null)[]> {
+  return Promise.all(ids.map(id => getActress(id)))
+}
+
